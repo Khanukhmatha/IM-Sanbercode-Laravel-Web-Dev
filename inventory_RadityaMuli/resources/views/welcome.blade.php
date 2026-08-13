5c096c9689efdddf07a395189d1415e4190d983c
Raditya Muli <raditya.muli06@gmail.com>
2026-08-13 12:37:00 +0700

    Tugas 13

--- inventory_RadityaMuli/resources/views/welcome.blade.php
+++ inventory_RadityaMuli/resources/views/welcome.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Selamat Datang</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('title', 'Welcome Page')
+@endsection
+@section('content')
     <!-- Menampilkan variabel yang dikirim dari FormController -->
     <h1>Selamat Datang, {{ $firstName }} {{ $lastName }}!</h1>
     <h3>Terima kasih telah bergabung.</h3>
-</body>
-</html>
+@endsection
--- inventory_RadityaMuli/routes/web.php
+++ inventory_RadityaMuli/routes/web.php
@@ -8,3 +8,9 @@ use App\Http\Controllers\FormController;
 Route::get('/', [DashboardController::class, 'home']);
 Route::get('/register', [FormController::class, 'register']);
 Route::post('/welcome', [FormController::class, 'welcome']);
+
+
+//testing
+Route::get('/master', function () {
+    return view('layouts.master');
+});
